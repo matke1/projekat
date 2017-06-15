0014d774b3b6c33899c930228b1f3d3462bb4558
matke1 <matke11.mm@gmail.com>
2017-06-15 15:05:36 +0200

    Prvi commit

---
+++ vars/config.rb
@@ -0,0 +1,12 @@
+require 'sass-globing'
+require 'singularitygs'
+
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "sass"
+css_dir = "css"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
